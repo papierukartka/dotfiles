35960b50cf9e95d5e6f86dcece8accfd3daed16d
Ś, Krzysztof <papierukartka@gmail.com>
2020-11-13 14:55:42 +0100

    [qutebrowser] html.duckduckgo as a search engine and start page

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,3 +2,7 @@ config.load_autoconfig()
 
 c.content.cookies.accept = 'no-3rdparty'
 c.content.cookies.store = False
+
+c.url.default_page = 'https://html.duckduckgo.com/'
+c.url.searchengines = {"DEFAULT": "https://html.duckduckgo.com/?q={}"}
+c.url.start_pages = 'https://html.duckduckgo.com'
