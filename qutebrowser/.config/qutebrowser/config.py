10ce7aeed9ca89676d27ba3a453c8015c89536a1
Ś, Krzysztof <papierukartka@gmail.com>
2020-12-04 11:14:04 +0100

    [qutebrowser] change search url in one case

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,5 +4,5 @@ c.content.cookies.accept = 'no-3rdparty'
 c.content.cookies.store = False
 
 c.url.default_page = 'https://html.duckduckgo.com/'
-c.url.searchengines = {"DEFAULT": "https://html.duckduckgo.com/?q={}"}
+c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}"}
 c.url.start_pages = 'https://html.duckduckgo.com'
